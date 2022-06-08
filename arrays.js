ebb56b0f5aa21f3d1fe5c1f391991007b7076044
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-06-08 11:49:37 -0600

    Added Statement corrections

--- arrays.js
+++ arrays.js
@@ -7,8 +7,8 @@ var hungryStudents= [notHungry , hungry , hungry, notHungry]
 
 students.pop() //Hazel will be removed at the end of the element
 donuts.push(1) //1 donuts will be added to the end of the element
-hungryStudents.unshift(hungry)// hungry or true will be added to the first index
-                              //in the element
+hungryStudents.unshift(hungry)// True will be added to the first index
+                              //in the element when printed
 
 
 //Index positions: Index positions are where something is located in an element
