2785d1c9a2182dc33f8c807bacb8bb9773f85ea0
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-06-08 11:37:19 -0600

    Add arrays

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var hungry= "true"
+var notHungry= "false"
+var students = ["Shaya" , "Jake" , "Angie" ," Hazel"];
+var donuts= [7 , 4 , 2 , 5]
+var hungryStudents= [notHungry , hungry , hungry, notHungry]
+
+
+students.pop() //Hazel will be removed at the end of the element
+donuts.push(1) //1 donuts will be added to the end of the element
+hungryStudents.unshift(hungry)// hungry or true will be added to the first index in the element
+
+
+
+
+
+
+console.log(students);
+console.log(donuts);
+console.log(hungryStudents);
